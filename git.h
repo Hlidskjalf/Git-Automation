83ddbbe4ade4cb79a6868445a9342758a4c2a757
Hlidskjalf <vroman197@gmail.com>
2017-08-12 14:35:33 -0400

    Assisted push

--- git.h
+++ git.h
@@ -1,11 +1,10 @@
 #ifndef GIT_H
 #define GIT_H
 
-void git_cmd_set_upstream();
-void git_cmd_init();
-void git_cmd_add();
-void git_cmd_commit();
-void git_cmd_push();
-void git_cmd_push();
+int git_cmd_init();
+int git_cmd_add();
+int git_cmd_commit();
+int git_cmd_push();
+int git_cmd_push();
 
 #endif // GIT_H
