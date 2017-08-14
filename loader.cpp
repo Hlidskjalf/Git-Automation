c3b13c6cfe3a423579e0bc501e606917b94bcaa1
Hlidskjalf <vroman197@gmail.com>
2017-08-14 10:01:01 -0400

    Assisted push

--- loader.cpp
+++ loader.cpp
@@ -1,1 +1,2 @@
 #include <iostream>int main(int argc, char **argv) {}#include <iostream>int main(int argc, char **argv) {}#include <iostream>int main(int argc, char **argv) {}987*^#!<>/^/#$%*&!)!@@#$qAROIAavARGRG955%^%236()@5134162
+987*^#!<>/^/#$%*&!)!@@#$qAROIAavARGRG955%^%236()@5134162
