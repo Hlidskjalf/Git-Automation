4a9985f3ddc8c126f74ebb7368a4fa1a2e7b3fe5
Hlidskjalf <vroman197@gmail.com>
2017-08-24 08:32:36 -0400

    Inital commit

--- loader.cpp
+++ loader.cpp
@@ -1,3 +1,4 @@
+35^57#&^$87^4%#8*&^53$@^#&$*&(%8&*6&676*&9*0^9*&65^7#8$0
 #include <iostream>int main(int argc, char **argv) {}#include <iostream>int main(int argc, char **argv) {}#include <iostream>int main(int argc, char **argv) {}987*^#!<>/^/#$%*&!)!@@#$qAROIAavARGRG955%^%236()@5134162
 987*^#!<>/^/#$%*&!)!@@#$qAROIAavARGRG955%^%236()@5134162
 987*^#!<>/^/#$%*&!)!@@#$qAROIAavARGRG955%^%236()@5134162
